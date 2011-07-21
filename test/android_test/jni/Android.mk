8131ec06ddfc50fe35654e9b15af50789efca8bc
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-07-21 14:58:42 +0000

    * share cpp files

--- test/android_test/jni/Android.mk
+++ test/android_test/jni/Android.mk
@@ -7,13 +7,13 @@ include $(LOCAL_PATH)/../../../build/android/jni/Android.mk
 LOCAL_PATH := $(MY_LOCAL_PATH)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE:= android_test
+LOCAL_MODULE := android_test
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include
 
 LOCAL_SRC_FILES := \
 		Main.cpp \
-		GameApp.cpp \
+		../../source/GameApp.cpp \
 
 LOCAL_LDLIBS := -lz -lm -llog -lGLESv2
 

7be70c57f82471d14fa89984589aec1739039e08
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-07-21 09:16:53 +0000

    + OpenGL ES 2.0 for windows

--- build/android/jni/Android.mk
+++ build/android/jni/Android.mk
@@ -1,30 +1,26 @@
-LOCAL_PATH := $(call my-dir)
+# Build Static lib
 
-# Define vars for library that will be build statically.
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := spank
+
+LOCAL_MODULE := libspank
+
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include
+
 LOCAL_SRC_FILES := \
 		FileMgr_Impl.cpp \
 		DebugUtil_Impl.cpp \
-		Main.cpp \
 		Renderer_Impl.cpp \
-		GameMain_Impl.cpp \
 		unzip/unzip.cpp \
 		unzip/ioapi.cpp \
 		../../../source/IGameMain.cpp \
 		../../../source/util/StreamReader.cpp \
 		../../../source/util/StreamWriter.cpp \
 
-# Optional compiler flags.
-# LOCAL_LDLIBS   = -lz -lm
-# LOCAL_CFLAGS   = -Wall -pedantic -std=c99 -g
+#LOCAL_LDLIBS :=
+
+#LOCAL_STATIC_LIBRARIES :=
+
+#LOCAL_SHARED_LIBRARIES :=
 
 include $(BUILD_STATIC_LIBRARY)
-
-# First lib, which will be built statically.
-include $(CLEAR_VARS)
-LOCAL_MODULE := main
-LOCAL_STATIC_LIBRARIES := spank
-
-include $(BUILD_SHARED_LIBRARY)

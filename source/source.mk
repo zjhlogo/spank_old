29f872f31d2155309f8d6e4f6bfad4de95c3e6cb
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-07-23 07:48:28 +0000

    + render device + config mgr

--- source/source.mk
+++ source/source.mk
@@ -4,9 +4,9 @@ LIBSPANK_SOURCE_FILES := \
 		$(LIBSPANK_SOURCE)/IGameMain.cpp \
 		$(LIBSPANK_SOURCE)/util/StreamReader.cpp \
 		$(LIBSPANK_SOURCE)/util/StreamWriter.cpp \
-		$(LIBSPANK_SOURCE)/../build/android/jni/DebugUtil_Impl.cpp \
-		$(LIBSPANK_SOURCE)/../build/android/jni/FileMgr_Impl.cpp \
-		$(LIBSPANK_SOURCE)/../build/android/jni/RenderDevice_Impl.cpp \
-		$(LIBSPANK_SOURCE)/../build/android/jni/TextureMgr_Impl.cpp \
-		$(LIBSPANK_SOURCE)/../build/android/jni/unzip/unzip.cpp \
-		$(LIBSPANK_SOURCE)/../build/android/jni/unzip/ioapi.cpp \
+		$(LIBSPANK_SOURCE)/../build/android/DebugUtil_Impl.cpp \
+		$(LIBSPANK_SOURCE)/../build/android/FileMgr_Impl.cpp \
+		$(LIBSPANK_SOURCE)/../build/android/RenderDevice_Impl.cpp \
+		$(LIBSPANK_SOURCE)/../build/android/TextureMgr_Impl.cpp \
+		$(LIBSPANK_SOURCE)/../build/android/unzip/unzip.cpp \
+		$(LIBSPANK_SOURCE)/../build/android/unzip/ioapi.cpp \

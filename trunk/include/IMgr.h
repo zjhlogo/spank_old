767f7e262a3490b1ea33fbf4e668e9d9b473349e
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-08-12 07:48:45 +0000

    + document the classes

--- trunk/include/IMgr.h
+++ trunk/include/IMgr.h
@@ -10,10 +10,29 @@
 
 #include "msg/IMsgHandler.h"
 
+/*! \brief base class of all management classes
+ *
+ * all managerment classes should inheritance from IMgr to initialize itself by
+ * override the pure virtual function Initialize(), and terminate the classse
+ * by override the pure virtual function Terminate().
+ */
 class IMgr : public IMsgHandler
 {
 public:
+	
+	/*!
+	 * \brief initialize the instance
+	 *
+	 * initialize all data (ex. memory allocate, ...) inside the function
+	 * \return if initialize success return true, otherwise return false
+	 */
 	virtual bool Initialize() = 0;
+	
+	/*!
+	 * \brief terminate the instance
+	 *
+	 * release all data (ex. memory release, ...) inside the function
+	 */
 	virtual void Terminate() = 0;
 
 };

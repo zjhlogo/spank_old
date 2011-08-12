ffb507f1e1b8b54fef87f7f82dd51deffd34f3e8
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-08-12 07:48:45 +0000

    + document the classes

--- include/msg/MsgMgr.h
+++ include/msg/MsgMgr.h
@@ -14,50 +14,123 @@
 #include <map>
 #include <queue>
 
+/*! \brief a message managerment class
+ *
+ * MsgMgr used for handler and/or dispatch the message event form one object to
+ * anthor object (Inheritanced from IMsgHandler). use SubscribeMessage() to
+ * subscribe and handling the message. use UnsubscribeMessage() to un-subscribe
+ * and un-handling the message. use SendMessage() to generate a message and send
+ * it to objects who handling this message. (note: the message will not send 
+ * immidielity, it's going to send just before the next IGameApp update (IGameApp::Update)).
+ */
 class MsgMgr : public IMgr
 {
 public:
+	/*! message subscribe information */
 	typedef struct SUBSCRIBE_INFO_tag
 	{
-		uint nMsgID;
-		IMsgHandler* pHandler;
-		MSG_CALLBACK pCallback;
+		uint nMsgID;					/*!< message identity \see MESSAGE_ID */
+		IMsgHandler* pHandler;			/*!< message handler, the IMsgHandler who is going to handle the message */
+		MSG_CALLBACK pCallback;			/*!< the callback function to receive the message */
 	} SUBSCRIBE_INFO;
 
+	/*! message subscribe information map */
 	typedef std::multimap<uint, SUBSCRIBE_INFO> TM_SUBSCRIBE_INFO;
 
+	/*! message object information */
 	typedef struct MESSAGE_INFO_tag
 	{
-		uint nMsgID;
-		IMsgBase* pMsg;
+		uint nMsgID;					/*!< message identity \see MESSAGE_ID */
+		IMsgBase* pMsg;					/*!< the message object */
 	} MESSAGE_INFO;
 
+	/*! message object information queue */
 	typedef std::queue<MESSAGE_INFO> TQ_MESSAGE_INFO;
 
 public:
 	MsgMgr();
 	virtual ~MsgMgr();
 
+	/*!
+	 * \brief get the instance of MsgMgr
+	 * \return the instance of the MsgMgr
+	 */
 	static MsgMgr& GetInstance();
 
 	virtual bool Initialize();
 	virtual void Terminate();
 
+	/*!
+	 * \brief send a message
+	 *
+	 * 
+	 * \param pMsg the message object to send
+	 */
 	void SendMessage(IMsgBase* pMsg);
+	
+	/*!
+	 * \brief dispatch the message to the handlers
+	 *
+	 * it is invoke by system before IGameApp update, it means all handlers
+	 * who subscribe this message will receive the message event before IGameApp
+	 * update. do not invoke this method manually, except you know what you are
+	 * doning.
+	 */
 	void DispatchMessage();
 
+	
+	/*!
+	 * \brief subscribe a message
+	 *
+	 * it means, to tell the MsgMgr that the object (pHandler) want to receive
+	 * the message whitch the message identity is nMsgID by calling it's member
+	 * function pCallback.
+	 * \param nMsgID the message identity
+	 * \param pHandler the handler object
+	 * \param pCallback the callback function
+	 * \return return true if subscribe success, otherwise return false
+	 */
 	bool SubscribeMessage(uint nMsgID, IMsgHandler* pHandler, MSG_CALLBACK pCallback);
+	
+	/*!
+	 * \brief un-subscribe a message
+	 * \param nMsgID the message identity
+	 * \param pHandler the handler object
+	 * \return return true if un-subscribe success, return false if can not find the 
+	 *         subscribe information of which message identity is nMsgID and handler
+	 *         is pHandler
+	 */
 	bool UnsubscribeMessage(uint nMsgID, IMsgHandler* pHandler);
+	
+	/*!
+	 * \brief un-subscribe all messages of a handler
+	 * \param pHandler the handler object
+	 * \return return true if un-subscribe success, otherwise return false
+	 */
 	bool UnsubscribeMessage(IMsgHandler* pHandler);
 
 private:
+	
+	/*!
+	 * \brief find the subscribe information
+	 * \param nMsgID the message identity \see MESSAGE_ID
+	 * \param pHandler the handler object
+	 * \return return the iterator of the TM_SUBSCRIBE_INFO, if not found return
+	 *         TM_SUBSCRIBE_INFO::end()
+	 */
 	TM_SUBSCRIBE_INFO::iterator FindSubscribeInfo(uint nMsgID, IMsgHandler* pHandler);
+	
+	/*!
+	 * \brief dispatch one message to handlers
+	 * \param msgInfo the message information to dispatch
+	 * \return return true if dispatch success, otherwise return false
+	 */
 	bool DispatchOneMessage(MESSAGE_INFO& msgInfo);
 
 private:
-	IMutex* m_pMutex;
-	TM_SUBSCRIBE_INFO m_SubscribeMsgMap;
-	TQ_MESSAGE_INFO m_MsgQueue;
+	IMutex* m_pMutex;						/*!< the mutex object to keep it thread safe */
+	TM_SUBSCRIBE_INFO m_SubscribeMsgMap;	/*!< the subscribe message map */
+	TQ_MESSAGE_INFO m_MsgQueue;				/*!< the message object queue for dispatch */
 
 };
 #endif // __MSGMGR_H__
--- include/util/ScreenUtil.h
+++ include/util/ScreenUtil.h
@@ -11,41 +11,103 @@
 #include "../IMgr.h"
 #include "../math/Vector2.h"
 
+/*! \brief screen coordinate system utility class
+ *
+ * it is a utility class to manage the screen coordinate system. use this class
+ * to convert device coordinate to screen coordinate.
+ */
 class ScreenUtil : public IMgr
 {
 public:
+	/*! screen rotation enumlate */
 	enum SCREEN_ROTATION
 	{
-		SR_NONE = 0,
-		SR_P90,
-		SR_N90,
-		SR_180,
-		NUM_SR,
+		SR_NONE = 0,		/*!< un-rotated, default portrait mode */
+		SR_P90,				/*!< rotate screen +90 degree */
+		SR_N90,				/*!< rotate screen -90 degree */
+		SR_180,				/*!< rotate screen +180 degree */
+		NUM_SR,				/*!< numbers of screen rotation enumlation */
 	};
 
 public:
+	
+	/*!
+	 * \brief default constructure
+	 */
 	ScreenUtil();
+	
+	/*!
+	 * \brief default destructure
+	 */
 	virtual ~ScreenUtil();
 
+	
+	/*!
+	 * \brief get the instance of ScreenUtil
+	 * \return the instance of ScreenUtil
+	 */
 	static ScreenUtil& GetInstance();
 
 	virtual bool Initialize();
 	virtual void Terminate();
 
+	
+	/*!
+	 * \brief get the screen width (after rotation)
+	 * \return the screen width
+	 */
 	int GetScreenWidth() const;
+	
+	/*!
+	 * \brief get the screen height (after rotation)
+	 * \return the screen height
+	 */
 	int GetScreenHeight() const;
 
+	
+	/*!
+	 * \brief get the screen rotation
+	 * \return the screen rotation \see SCREEN_ROTATION
+	 */
 	SCREEN_ROTATION GetScreenRotation() const;
+	
+	/*!
+	 * \brief get the screen rotation degree
+	 *
+	 * for example if screen rotate by SR_P90 the return value will be -90.0f, 
+	 * because the screen rotate by +90.0f, coordinate y will point to left
+	 * for keeping the coordinate y up, rotate it back -90.0f degreed. so it 
+	 * return the invert of the rotation.
+	 * \return the screen rotation degree
+	 */
 	float GetRotationDegree() const;
 
+	
+	/*!
+	 * \brief convert device position to screen position
+	 * \param vPosOut output vector
+	 * \param x device position x
+	 * \param y device position y
+	 */
 	void DevicePointToScreen(Vector2& vPosOut, float x, float y);
+	
+	/*!
+	 * \brief convert device position to screen position
+	 * \param vPosOut output vector
+	 * \param pos device position
+	 */
 	void DevicePointToScreen(Vector2& vPosOut, const Vector2& pos);
+	
+	/*!
+	 * \brief convert device position to screen position
+	 * \param vPosInOut device position for input and output the screen position
+	 */
 	void DevicePointToScreen(Vector2& vPosInOut);
 
 private:
-	int m_nScreenWidth;
-	int m_nScreenHeight;
-	SCREEN_ROTATION m_eScreenRotation;
+	int m_nScreenWidth;						/*!< screen width */
+	int m_nScreenHeight;					/*!< screen height */
+	SCREEN_ROTATION m_eScreenRotation;		/*!< screen rotation */
 
 };
 #endif // __SCREENUTIL_H__

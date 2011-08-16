ea7b483ea38b50e3ff895ebe4039cd6f891a03d3
wbaoqing <wbaoqing@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-08-16 07:03:31 +0000

    *Modify Level2D File *Modify Update *Modify Render change m_CenterPostion to the m_nCenterPostionX and m_nCenterPositionY

--- trunk/source/Level2D_Impl.cpp
+++ trunk/source/Level2D_Impl.cpp
@@ -11,46 +11,172 @@
 #include <ITextureMgr.h>
 #include <IRenderer2D.h>
 #include <tinyxml-2.6.2/tinyxml.h>
-Level2D_Impl::Level2D_Impl()
+#include <Math/IMath.h>
+#include <util/ScreenUtil.h>
+Level2D_Impl::Level2D_Impl(const char* pszLevel2DFile)
 {
-	// TODO:
+
+
 	m_CenterPosition.x = 0;
 	m_CenterPosition.y = 0;
 	
-	m_GidAry = NULL;
-	m_LayerWidth = 0;
-	m_LayerHeight = 0;
+	m_TexturePosition = m_CenterPosition;
 	
-	m_TileWidth = 0;
-	m_TildHeight = 0;
+	m_nCenterPositionX = 0;
+	m_nCenterPositionY = 0;
+	m_nPrvCenterPositionX = 0;
+	m_nPrvCenterPositionY = 0;
+	
+	m_nCurrenSurfaceOffX = 0;
+	m_nCurrenSurfaceOffY = 0;
+	
+	m_nLayerWidth = 0;
+	m_nLayerHeight = 0;
+
+	m_nTileWidth = 0;
+	m_nTildHeight = 0;
+
+	IMath::BuildIdentityMatrix(m_ModleMatrix);
+	m_nHalfSceneHeight = ScreenUtil::GetInstance().GetScreenHeight()/2;
+	m_nHalfSceneWidth = ScreenUtil::GetInstance().GetScreenWidth()/2;
+	m_nHalfBuffersize = 3;
+	
+	m_pGidAry = NULL;
 	m_pTexture = NULL;
 	m_pVerts = NULL;
+	m_pShader = NULL;
+	
+	m_bOK = LoadLevel2DFromFile(pszLevel2DFile);
 }
 
 Level2D_Impl::~Level2D_Impl()
 {
 	// TODO: 
 	SAFE_DELETE(m_pTexture);
-	SAFE_DELETE_ARRAY(m_GidAry);
+	SAFE_DELETE_ARRAY(m_pGidAry);
 	SAFE_DELETE_ARRAY(m_pVerts);
+	SAFE_DELETE(m_pShader);
 }
-
+void Level2D_Impl::Terminate()
+{
+	SAFE_DELETE_ARRAY(m_pVerts);
+	SAFE_DELETE_ARRAY(m_pGidAry);
+	SAFE_DELETE(m_pTexture);
+	SAFE_DELETE(m_pShader);
+}
+ILevel2D* ILevel2D::CreateLevel2D(const char* pszLevel2DFile)
+{
+	Level2D_Impl* pLevel2D =  new Level2D_Impl(pszLevel2DFile);
+	if (!pLevel2D || !pLevel2D->IsOK())
+	{
+		SAFE_DELETE(pLevel2D);
+		return NULL;
+	}
+	return pLevel2D;
+}
 void Level2D_Impl::Update(float dt)
 {
-	// TODO: 
+	// TODO:
+	
+	if(m_nCenterPositionX == m_nPrvCenterPositionX && m_nCenterPositionY == m_nPrvCenterPositionY)
+		return;
+
+	int nXBoundary = (m_nHalfBuffersize + (m_nHalfSceneWidth/m_nTileWidth)) * m_nTileWidth - m_nHalfSceneWidth;
+	int nYBoundary = (m_nHalfBuffersize + (m_nHalfSceneHeight/m_nTildHeight)) * m_nTildHeight - m_nHalfSceneHeight;
+
+	if((m_nCenterPositionX - m_nPrvCenterPositionX > nXBoundary)
+		||(m_nCenterPositionX - m_nPrvCenterPositionX < -nXBoundary)
+		||((m_nCenterPositionY - m_nPrvCenterPositionY) > nYBoundary)
+		||(m_nCenterPositionY - m_nPrvCenterPositionY < -nYBoundary))
+	{
+		int TranslateX = 0;
+		int TranslateY = 0;
+		m_TexturePosition.x = (m_CenterPosition.x / m_nTileWidth) * m_nTileWidth;
+		m_TexturePosition.y = (m_CenterPosition.y/ m_nTildHeight) * m_nTildHeight;
+		
+		TranslateX = m_nCenterPositionX - (int)m_TexturePosition.x;
+		TranslateY = m_nCenterPositionY - (int)m_TexturePosition.y;
+
+		m_nCurrenSurfaceOffX = TranslateX;
+		m_nCurrenSurfaceOffY = TranslateY;
+		InitVerts();
+		IMath::BuildIdentityMatrix(m_ModleMatrix);
+		Matrix4x4 TranlateMatrix;
+		IMath::BuildIdentityMatrix(TranlateMatrix);
+		TranlateMatrix.Translate(-TranslateX,-TranslateY, 0);
+		m_ModleMatrix *= TranlateMatrix;	
+	}
+	else
+	{
+		//Translate
+		int TranslateX = 0;
+		int TranslateY = 0;
+		TranslateX =  m_nCenterPositionX - m_nPrvCenterPositionX;
+		TranslateY =  m_nCenterPositionY - m_nPrvCenterPositionY;
+		m_nCurrenSurfaceOffX += TranslateX;
+		m_nCurrenSurfaceOffY += TranslateY;
+
+		if((m_nCurrenSurfaceOffX> nXBoundary) 
+			|| (m_nCurrenSurfaceOffX < -nXBoundary)
+			|| (m_nCurrenSurfaceOffY >nYBoundary)
+			|| (m_nCurrenSurfaceOffY < -nYBoundary))
+		{
+			m_TexturePosition.x = float(m_nCenterPositionX / m_nTileWidth) * m_nTileWidth;
+			m_TexturePosition.y = float(m_nCenterPositionY / m_nTildHeight) * m_nTildHeight;
+			TranslateX = m_nCenterPositionX - (int)m_TexturePosition.x;
+			TranslateY = m_nCenterPositionY - (int)m_TexturePosition.y;
+
+			m_nCurrenSurfaceOffX= TranslateX;
+			m_nCurrenSurfaceOffY = TranslateY;
+
+			InitVerts();
+			IMath::BuildIdentityMatrix(m_ModleMatrix);
+			Matrix4x4 TranlateMatrix;
+			IMath::BuildIdentityMatrix(TranlateMatrix);
+			TranlateMatrix.Translate(-TranslateX, -TranslateY, 0);
+			m_ModleMatrix *= TranlateMatrix;
+		}
+		else
+		{
+			Matrix4x4 TranlateMatrix;
+			IMath::BuildIdentityMatrix(TranlateMatrix);
+			TranlateMatrix.Translate(-TranslateX, -TranslateY, 0);
+			m_ModleMatrix *= TranlateMatrix;
+			
+		}
+	}
+	m_nPrvCenterPositionX = m_nCenterPositionX;
+	m_nPrvCenterPositionY = m_nCenterPositionY;
+	
 }
 
 void Level2D_Impl::Render()
 {
-	// TODO: 
-	CreateVertexs();
+	// TODO: set matrix
+	m_pShader->SetTexture("u_texture",m_pTexture);
+
+	IRenderer2D::GetInstance().SetModelViewMatrix(m_ModleMatrix);
+	m_pShader->SetMatrix4x4("u_matModelViewProj", IRenderer2D::GetInstance().GetFinalMatrixTranspose());
+
+	int nXTileSize =(m_nHalfSceneWidth / m_nTileWidth) * 2 + m_nHalfBuffersize * 2;	
+	int nYTileSize = (m_nHalfSceneHeight / m_nTildHeight) * 2+m_nHalfBuffersize * 2;
+	for(int y = 0; y < nYTileSize; y++)
+	{	
+		for (int x =0; x <nXTileSize; x++)
+		{
+			int index = y*nXTileSize +x;
+			IRenderer2D::GetInstance().DrawRect(&m_pVerts[index * 4], m_pShader);
+		}
+	}
 	
 }
 
 void Level2D_Impl::SetCenterPosition(const Vector2& pos)
 {
-	// TODO: 
+	// TODO:
 	m_CenterPosition = pos;
+	m_nCenterPositionX = (int)m_CenterPosition.x;
+	m_nCenterPositionY = (int)m_CenterPosition.y;
 }
 
 const Vector2& Level2D_Impl::GetCenterPosition() const
@@ -61,22 +187,28 @@ const Vector2& Level2D_Impl::GetCenterPosition() const
 
 bool Level2D_Impl::LoadLevel2DFromFile(const char* pszLevel2DFile)
 {
-	//TODO:
+
+	// Parse The MapEditor XML File
 	StreamReader* pReader = IFileUtil::GetInstance().LoadFile(pszLevel2DFile);
 	if (!pReader) return false;
 
 	TiXmlDocument doc;
+	doc.Parse((const char*)pReader->GetBuffer());
+	SAFE_RELEASE(pReader);
+
+	if (doc.Error()) return false;
 	TiXmlElement* pElemTild = doc.RootElement();
 
 
-	pElemTild->Attribute("width",&m_LayerWidth);
-	if(m_LayerWidth <= 0 )return false;
-	pElemTild->Attribute("height",&m_LayerHeight);
-	if(m_LayerHeight<= 0) return false;
-	pElemTild->Attribute("tilewidth",&m_TileWidth);
-	if (m_TileWidth <= 0) return false;
-	pElemTild->Attribute("tileheight",&m_TildHeight);
-	if(m_TildHeight<= 0)return false;
+	pElemTild->Attribute("width",&m_nLayerWidth);
+	if(m_nLayerWidth <= 0 )return false;
+	pElemTild->Attribute("height",&m_nLayerHeight);
+	if(m_nLayerHeight<= 0) return false;
+	pElemTild->Attribute("tilewidth",&m_nTileWidth);
+	if (m_nTileWidth <= 0) return false;
+	pElemTild->Attribute("tileheight",&m_nTildHeight);
+	if(m_nTildHeight<= 0)return false;
+	
 
 	TiXmlElement* LayerSet = pElemTild->FirstChildElement();
 	LayerSet = LayerSet->FirstChildElement();
@@ -85,9 +217,7 @@ bool Level2D_Impl::LoadLevel2DFromFile(const char* pszLevel2DFile)
 	pszTexture = LayerSet->Attribute("source");
 	
 	if(pszTexture == NULL) return false;
-	//load Min texture
-	m_pTexture = ITextureMgr::GetInstance().CreateTexture(pszTexture);
-	
+
 	TiXmlElement* layer = pElemTild->FirstChildElement("layer");
 	TiXmlElement* layerData = layer->FirstChildElement("data");
 	
@@ -96,32 +226,107 @@ bool Level2D_Impl::LoadLevel2DFromFile(const char* pszLevel2DFile)
 	TiXmlElement* Data = layerData->FirstChildElement();
 	if(!Data) return false;
 	
-	SAFE_DELETE_ARRAY(m_GidAry);
-	m_GidAry = new int[m_LayerWidth * m_LayerHeight];
+	SAFE_DELETE_ARRAY(m_pGidAry);
+	m_pGidAry = new int[m_nLayerWidth * m_nLayerHeight];
 	
 	int temp = 0;
 	int index = 0;
-	do 
+	while (Data)
 	{
 		Data->Attribute("gid",&temp);
-		//µØÍ¼ÎÆÀí¿éË÷Òý
-		m_GidAry[index] = temp;
+		m_pGidAry[index] = temp;
 		index++;
 		Data = Data->NextSiblingElement();
-	} while (Data);
+	}
+	//Init Shader And TextTrue;
+	m_pShader = IShaderMgr::GetInstance().CreateShader("default_shader.xml");
+	if(!m_pShader) return false;
 
+	m_pTexture = ITextureMgr::GetInstance().CreateTexture("isometric_tileset.png");
+	if(NULL == m_pTexture) return false;
+	//Init Render Verts
+	SAFE_DELETE_ARRAY(m_pVerts);
+	int nXTileSize =(m_nHalfSceneWidth / m_nTileWidth) * 2 + m_nHalfBuffersize * 2;	
+	int nYTileSize = (m_nHalfSceneHeight / m_nTildHeight) * 2+m_nHalfBuffersize * 2;
+	m_pVerts = new VERTEX_ATTRIBUTE[nXTileSize * nYTileSize * 4 ];
+	memset(m_pVerts,0,nYTileSize * nXTileSize * 4 *sizeof(VERTEX_ATTRIBUTE));
+	InitVerts();
 	return true;
 
 }
-void Level2D_Impl::CreateVertexs()
+void Level2D_Impl::InitVerts()
 {
-	//TODO:
-	SAFE_DELETE_ARRAY(m_pVerts);
-	int ScenWidth = 800;
-	int ScenHeight = 480;
+	int nXTileSize =(m_nHalfSceneWidth / m_nTileWidth) * 2 + m_nHalfBuffersize * 2;	
+	int nYTileSize = (m_nHalfSceneHeight / m_nTildHeight) * 2 + m_nHalfBuffersize * 2;
+	//Init Start Coordinate
+	float fXstartCoord = -float(nXTileSize / 2) * m_nTileWidth;
+	float fYstartCoord =  float(nYTileSize / 2) * m_nTildHeight;
+	//TextTrue off 
+	float fTextTrueXOff = (float)m_nTileWidth / m_pTexture->GetWidth();
+	float fTextTrueYOff = (float)m_nTildHeight/ m_pTexture->GetHeight();
 
-	int Xsize = ScenWidth / m_TileWidth;
-	int Ysize = ScenHeight / m_TildHeight;
-	m_pVerts = new VERTEX_ATTRIBUTE[Ysize * Xsize * 4*4];
+	Vector2 TextTrueCoordinate;
 	
+	for(int v1=0; v1 < nYTileSize; v1++)
+	{
+		for ( int v2=0; v2 < nXTileSize; v2++ )
+		{
+			int noff = v1 * nXTileSize *4 +	v2 * 4;
+			Vector2 TextTrueCoordinate;
+			TextTrueCoordinate.x = fXstartCoord + m_TexturePosition.x;
+			TextTrueCoordinate.y = fYstartCoord + m_TexturePosition.y;
+			C2TextTureCoordinate(TextTrueCoordinate);
+			m_pVerts[noff].x = fXstartCoord;
+			m_pVerts[noff].y = fYstartCoord - m_nTildHeight;
+			m_pVerts[noff].z = 0.0f;
+			m_pVerts[noff].u = TextTrueCoordinate.x;
+			m_pVerts[noff].v = TextTrueCoordinate.y - fTextTrueYOff;
+
+			m_pVerts[noff +1].x = fXstartCoord;
+			m_pVerts[noff +1].y = fYstartCoord;
+			m_pVerts[noff +1].z = 0.0f;
+			m_pVerts[noff +1].u = TextTrueCoordinate.x;
+			m_pVerts[noff +1].v = TextTrueCoordinate.y;
+
+			m_pVerts[noff +2].x = fXstartCoord + m_nTileWidth;
+			m_pVerts[noff +2].y = fYstartCoord - m_nTildHeight;
+			m_pVerts[noff +2].z = 0.0f;
+			m_pVerts[noff +2].u = TextTrueCoordinate.x + fTextTrueXOff;
+			m_pVerts[noff +2].v = TextTrueCoordinate.y - fTextTrueYOff; 
+
+			m_pVerts[noff +3].x = fXstartCoord + m_nTileWidth;
+			m_pVerts[noff +3].y = fYstartCoord;
+			m_pVerts[noff +3].z = 0.0f;
+			m_pVerts[noff +3].u = TextTrueCoordinate.x + fTextTrueXOff;
+			m_pVerts[noff +3].v = TextTrueCoordinate.y;
+
+			//Add  a off distance to iXstarCoord
+			fXstartCoord += m_nTileWidth;
+			
+		}
+		//go to next Row, add a off distance to iYstarCoord
+		fXstartCoord = - (float)(nXTileSize / 2) * m_nTileWidth;
+		fYstartCoord -= m_nTildHeight;
+	}
+}
+
+Vector2& Level2D_Impl::C2TextTureCoordinate(Vector2& MapPosition)
+{
+	MapPosition.x += m_nLayerWidth * m_nTileWidth / 2;
+    MapPosition.y = m_nLayerHeight * m_nTildHeight/ 2 - MapPosition.y;   
+	
+	int indexX = (int)MapPosition.x / m_nTileWidth;
+	int indexY = (int)MapPosition.y/ m_nTildHeight;
+	int index = m_pGidAry[indexY * m_nLayerWidth + indexX];
+	
+	int nTextTrueXWidth = m_pTexture->GetWidth();
+	int  nTextTrueXHeight =m_pTexture->GetHeight();
+	
+	index--;
+	indexX = (index%2) * m_nTildHeight;
+	indexY = ((index/2) * m_nTileWidth); 
+
+	MapPosition.x=(float)indexX / nTextTrueXWidth;
+	MapPosition.y = 1.0f -(float)indexY/ nTextTrueXHeight;
+	return MapPosition;
 }

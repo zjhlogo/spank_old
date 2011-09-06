594d0322190ae464cc1537f1b4afeefba22aab9f
wbaoqing <wbaoqing@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-09-06 09:12:04 +0000

    *Modify the default_shader.fs

--- test/resources/default_shader.fs
+++ test/resources/default_shader.fs
@@ -6,6 +6,6 @@ varying vec2 v_texCoord;
 
 void main()
 {
-	// gl_FragColor = texture2D(u_texture, v_texCoord);
-	gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+	 gl_FragColor = texture2D(u_texture, v_texCoord);
+		//gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 }

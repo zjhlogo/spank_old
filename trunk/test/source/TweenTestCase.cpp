dc9ce2a4acb12b7542c88307ccd60f8352b9f844
wbaoqing@gmail.com <wbaoqing@gmail.com@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-09-19 14:14:08 +0000

    *modify TweenTestCase

--- trunk/test/source/TweenTestCase.cpp
+++ trunk/test/source/TweenTestCase.cpp
@@ -21,6 +21,7 @@
 #include <action/ActionElasticMoveTo.h>
 #include <action/ActionBounceMoveTo.h>
 #include <ui/UIButton.h>
+#include <ui/UIRadioButton.h>
 #include <math.h>
 #include <util/IDebugUtil.h>
 #include <ui/uimsg/UIMsgID.h>
@@ -29,6 +30,8 @@ TweenTestCase::TweenTestCase()
 {	
 	m_pSpriteAction = NULL;
 	m_pActionNode = NULL;
+	m_pActionSequeue = NULL;
+	m_pActionLoop = NULL;
 	m_nIndexTween = 0;
 	m_nEffectIndex = 0;
 }
@@ -40,18 +43,64 @@ TweenTestCase::~TweenTestCase()
 
 bool TweenTestCase::Initialize(UIScreen* pUIScreen)
 {
+	
+
 	m_pSpriteAction = new Sprite("test_ball.xml");
 	if (!m_pSpriteAction) return true;
 	m_pActionNode = ICore::GetInstance().GetRootNode()->CreateChildNode();
 	m_pActionNode->AttachObject(m_pSpriteAction);
 
-	ActionSequeue* pActionFregment = new ActionSequeue();
-	pActionFregment->AddAction(new ActionElasticMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
-	ActionSequeue* pActionSequeue = new ActionSequeue();
-	pActionSequeue->AddAction(pActionFregment);
 
-	ActionLoop* pActionLoop = new ActionLoop(pActionSequeue);
-	m_pActionNode->RunAction(pActionLoop);
+	m_pActionSequeue = new ActionSequeue();
+	
+
+	m_vActionLoop.push_back(new ActionElasticMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionElasticMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionElasticMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionBounceMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionBounceMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionBounceMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionCircMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionCircMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionCircMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionExpoMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionExpoMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionExpoMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionSineMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionSineMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionSineMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+
+	m_vActionLoop.push_back(new ActionQuintMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionQuintMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionQuintMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionQuartMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionQuartMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionQuartMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionCubicMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionCubicMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionCubicMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionBackMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionBackMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionBackMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	m_vActionLoop.push_back(new ActionQuadMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionQuadMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+	m_vActionLoop.push_back(new ActionQuadMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
+
+	
+	m_pActionSequeue->AddAction(m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]);
+	m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]->IncRef();
+	m_pActionLoop = new ActionLoop(m_pActionSequeue);
+	m_pActionNode->RunAction(m_pActionLoop);
+	
 	
 	UIButton* pBtRrvButton = new UIButton(pUIScreen);
 	pBtRrvButton->SetPosition(Vector2(10.0f, 10.0f));
@@ -61,7 +110,24 @@ bool TweenTestCase::Initialize(UIScreen* pUIScreen)
 	UIButton* pBtNextButton = new UIButton(pUIScreen);
 	pBtNextButton->SetPosition(Vector2(10.f, 60.0f));
 	pBtNextButton->SetText("Next");
-	pBtNextButton->ConnectEvent(UMI_CLICKED, this, CAST_MSG_CALLBACK(& TweenTestCase::OnNextButton));
+	pBtNextButton->ConnectEvent(UMI_CLICKED, this, CAST_MSG_CALLBACK(&TweenTestCase::OnNextButton));
+
+	UIRadioButton* pBtInButton = new UIRadioButton(pUIScreen);
+	pBtInButton->SetPosition(Vector2(200.0f, 10.0f));
+	pBtInButton->SetText("EASEIN");
+	pBtInButton->ConnectEvent(UMI_CHECKED, this, CAST_MSG_CALLBACK(&TweenTestCase::OnEASEInButton));
+
+	UIRadioButton* pBtOutButton = new UIRadioButton(pUIScreen);
+	pBtOutButton->SetPosition(Vector2(200.0f, 60.0f));
+	pBtOutButton->SetText("EASEOut");
+	pBtOutButton->ConnectEvent(UMI_CHECKED, this, CAST_MSG_CALLBACK(&TweenTestCase::OnEASEInOutButton));
+
+	UIRadioButton* pBtInOutButton = new UIRadioButton(pUIScreen);
+	pBtInOutButton->SetPosition(Vector2(200.0f, 110.0f));
+	pBtInOutButton->SetText("EASEInOut");
+	pBtInOutButton->ConnectEvent(UMI_CHECKED, this, CAST_MSG_CALLBACK(&TweenTestCase::OnEASEInOutButton));
+
+
 	return true;
 	
 
@@ -81,42 +147,57 @@ void TweenTestCase::Render()
 
 void TweenTestCase::OnPrvButton( IMsgBase* pMsg )
 {
-	LOGE("OnPrvButton Click");
+	LOGD("OnPrvButton Click");
 	if(((int)m_nIndexTween - 1) >= 0)
+	{
 		m_nIndexTween --;
-	LOGE("%d",m_nIndexTween);
+		UpdateTween();
+	}
 
 }
 
 void TweenTestCase::OnNextButton( IMsgBase* pMsg )
 {
-	const int MAX_SIZE = 6;
-	if((m_nIndexTween + 1) <= MAX_SIZE)
-		m_nIndexTween++;
-	LOGE("OnNextButton Click");
-	LOGE("%d",m_nIndexTween);
-}
-
-void TweenTestCase::ConnectAction( uint nIndex )
-{
-	ICore::GetInstance().GetRootNode()->RemoveChildNode(m_pActionNode);
-	ActionSequeue* pActionFregment = new ActionSequeue();
-	switch(nIndex)
+	
+	if((m_nIndexTween + 1) <= m_vActionLoop.size())
 	{
-	case 1:	
-		{
-			pActionFregment->AddAction(new ActionElasticMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
-			break;
-		}
-	case 2:
-		{
-			break;
-			//TODO:
-		}
+		m_nIndexTween++;
+		UpdateTween();
+		
 	}
-
-	ActionSequeue* pActionSequeue = new ActionSequeue();
-	pActionSequeue->AddAction(pActionFregment);
-	ActionLoop* pActionLoop = new ActionLoop(pActionSequeue);
-	m_pActionNode->RunAction(pActionLoop);
+	LOGD("OnNextButton Click");
 }
+
+void TweenTestCase::OnEASEInButton( IMsgBase* pMsg )
+{
+	if(m_nEffectIndex == 0) return;
+	m_nEffectIndex = 0;
+	UpdateTween();
+	LOGD("OnEASEInButton Click");
+}
+
+void TweenTestCase::OnEASEOutButton( IMsgBase* pMsg )
+{
+	if(m_nEffectIndex == 1) return;
+	m_nEffectIndex = 1;
+	UpdateTween();
+	LOGD("OnEASEOutButton Click");
+}
+
+void TweenTestCase::OnEASEInOutButton( IMsgBase* pMsg )
+{
+	if(m_nEffectIndex == 2)return;
+	m_nEffectIndex = 2;
+	UpdateTween();
+	LOGD("OnEASEInOutButton Click");
+}
+
+void TweenTestCase::UpdateTween()
+{
+	m_pActionSequeue = new ActionSequeue();
+	m_pActionSequeue->AddAction(m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]);
+	m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]->IncRef();
+	m_pActionLoop = new ActionLoop(m_pActionSequeue);
+	m_pActionNode->RunAction(m_pActionLoop);
+}
+

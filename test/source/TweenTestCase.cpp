dc8bf93e927c0403721fc448d926869bb2b0d06c
wbaoqing <wbaoqing@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-09-20 03:51:15 +0000

    *modify ActionSequeue add delete ConnectEvent in the desconstructor *modify the TweenTestCase Render

--- test/source/TweenTestCase.cpp
+++ test/source/TweenTestCase.cpp
@@ -20,25 +20,38 @@
 #include <action/ActionCircMoveTo.h>
 #include <action/ActionElasticMoveTo.h>
 #include <action/ActionBounceMoveTo.h>
+#include <ui/IRendererUI.h>
+#include <ui/IUIResMgr.h>
 #include <ui/UIButton.h>
 #include <ui/UIRadioButton.h>
 #include <math.h>
 #include <util/IDebugUtil.h>
 #include <ui/uimsg/UIMsgID.h>
+
+#include <IShaderMgr.h>
 TweenTestCase::TweenTestCase()
 :TestCase("TweenTestCase")
 {	
 	m_pSpriteAction = NULL;
 	m_pActionNode = NULL;
-	m_pActionSequeue = NULL;
-	m_pActionLoop = NULL;
+	m_pUiText = NULL;
+
 	m_nIndexTween = 0;
 	m_nEffectIndex = 0;
+	
 }
 
 TweenTestCase::~TweenTestCase()
 {
 	 // TODO: 
+	for (TV_ACTIONLOOP::iterator itr = m_vActionLoop.begin(); itr != m_vActionLoop.end(); itr++)
+	{
+		(*itr)->DisconnectEvent(MI_ACTION_UPDATE);
+		(*itr)->Release();
+		
+	}
+	m_vActionLoop.clear();
+
 }
 
 bool TweenTestCase::Initialize(UIScreen* pUIScreen)
@@ -49,10 +62,6 @@ bool TweenTestCase::Initialize(UIScreen* pUIScreen)
 	if (!m_pSpriteAction) return true;
 	m_pActionNode = ICore::GetInstance().GetRootNode()->CreateChildNode();
 	m_pActionNode->AttachObject(m_pSpriteAction);
-
-
-	m_pActionSequeue = new ActionSequeue();
-	
 
 	m_vActionLoop.push_back(new ActionElasticMoveTo(ATT_EASE_IN, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
 	m_vActionLoop.push_back(new ActionElasticMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
@@ -95,11 +104,14 @@ bool TweenTestCase::Initialize(UIScreen* pUIScreen)
 	m_vActionLoop.push_back(new ActionQuadMoveTo(ATT_EASE_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
 	m_vActionLoop.push_back(new ActionQuadMoveTo(ATT_EASE_IN_OUT, Vector3(-350.0f, 0.0f ,0.0f), Vector3(350.0f , 0.0f, 0.0f), 3.0f));
 
-	
-	m_pActionSequeue->AddAction(m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]);
+	char* psz_Text = "ActionElasticMoveTo";
+	m_pUiText = new UITextView(pUIScreen,psz_Text);
+	m_pUiText->SetPosition (Vector2(10.0f,340.0f));
+	ActionSequeue* pActionSequeue = new ActionSequeue();
+	pActionSequeue->AddAction(m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]);
 	m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]->IncRef();
-	m_pActionLoop = new ActionLoop(m_pActionSequeue);
-	m_pActionNode->RunAction(m_pActionLoop);
+	ActionLoop* pActionLoop = new ActionLoop(pActionSequeue);
+	m_pActionNode->RunAction(pActionLoop);
 	
 	
 	UIButton* pBtRrvButton = new UIButton(pUIScreen);
@@ -121,17 +133,13 @@ bool TweenTestCase::Initialize(UIScreen* pUIScreen)
 	UIRadioButton* pBtOutButton = new UIRadioButton(pUIScreen);
 	pBtOutButton->SetPosition(Vector2(200.0f, 60.0f));
 	pBtOutButton->SetText("EASEOut");
-	pBtOutButton->ConnectEvent(UMI_CHECKED, this, CAST_MSG_CALLBACK(&TweenTestCase::OnEASEInOutButton));
+	pBtOutButton->ConnectEvent(UMI_CHECKED, this, CAST_MSG_CALLBACK(&TweenTestCase::OnEASEOutButton));
 
 	UIRadioButton* pBtInOutButton = new UIRadioButton(pUIScreen);
 	pBtInOutButton->SetPosition(Vector2(200.0f, 110.0f));
 	pBtInOutButton->SetText("EASEInOut");
 	pBtInOutButton->ConnectEvent(UMI_CHECKED, this, CAST_MSG_CALLBACK(&TweenTestCase::OnEASEInOutButton));
-
-
 	return true;
-	
-
 }
 
 void TweenTestCase::Terminate()
@@ -142,39 +150,55 @@ void TweenTestCase::Terminate()
 }
 
 void TweenTestCase::Render()
-{
+{	
+	static VATTR_POS_UV Vers[2];
+	Vers[0].x = -350.f;
+	Vers[0].y = 0.0f;
+	Vers[0].z = 0.0f;
+	Vers[0].u = 0.0f;
+	Vers[0].v = 0.0f;
+
+	Vers[1].x = 350.f;
+	Vers[1].y = 0.0f;
+	Vers[1].z = 0.0f;
+	Vers[1].u = 0.0f;
+	Vers[1].v = 0.0f;
+	
+	static ushort Indis[2];
+	Indis[0] = 0;
+	Indis[1] = 1;
+	static IShader* pshader = IShaderMgr::GetInstance().CreateShader(SSI_DEFAULT);
+	pshader->SetMatrix4x4("u_matModelViewProj", IRenderer2D::GetInstance().GetFinalMatrixTranspose());
+	IRenderer2D::GetInstance().BeginRender();
+	IRenderer2D::GetInstance().DrawLineList(Vers, 2, Indis, 2, pshader);
+	IRenderer2D::GetInstance().EndRender();
 
 }
 
 void TweenTestCase::OnPrvButton( IMsgBase* pMsg )
 {
-	LOGD("OnPrvButton Click");
 	if(((int)m_nIndexTween - 1) >= 0)
 	{
 		m_nIndexTween --;
 		UpdateTween();
 	}
-
 }
 
 void TweenTestCase::OnNextButton( IMsgBase* pMsg )
 {
-	
-	if((m_nIndexTween + 1) <= m_vActionLoop.size())
+	if((m_nIndexTween + 1) < (m_vActionLoop.size() / TWEEN_TYPE_SIZE))
 	{
 		m_nIndexTween++;
 		UpdateTween();
-		
 	}
-	LOGD("OnNextButton Click");
 }
 
 void TweenTestCase::OnEASEInButton( IMsgBase* pMsg )
-{
+{ 
 	if(m_nEffectIndex == 0) return;
 	m_nEffectIndex = 0;
 	UpdateTween();
-	LOGD("OnEASEInButton Click");
+
 }
 
 void TweenTestCase::OnEASEOutButton( IMsgBase* pMsg )
@@ -182,7 +206,7 @@ void TweenTestCase::OnEASEOutButton( IMsgBase* pMsg )
 	if(m_nEffectIndex == 1) return;
 	m_nEffectIndex = 1;
 	UpdateTween();
-	LOGD("OnEASEOutButton Click");
+	
 }
 
 void TweenTestCase::OnEASEInOutButton( IMsgBase* pMsg )
@@ -190,15 +214,26 @@ void TweenTestCase::OnEASEInOutButton( IMsgBase* pMsg )
 	if(m_nEffectIndex == 2)return;
 	m_nEffectIndex = 2;
 	UpdateTween();
-	LOGD("OnEASEInOutButton Click");
+
 }
 
 void TweenTestCase::UpdateTween()
 {
-	m_pActionSequeue = new ActionSequeue();
-	m_pActionSequeue->AddAction(m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]);
+	const static char* s_pszText[] = {	"ActionElasticMoveTo",
+										"ActionBounceMoveTo",
+										"ActionCircMoveTo",
+										"ActionExpoMoveTo",
+										"ActionSineMoveTo",
+										"ActionQuintMoveTo",
+										"ActionQuartMoveTo",
+										"ActionCubicMoveTo",
+										"ActionBackMoveTo",
+										"ActionQuadMoveTo"};
+	m_pUiText->SetText(s_pszText[m_nIndexTween]);
+	ActionSequeue* pActionSequeue = new ActionSequeue();
+	pActionSequeue->AddAction(m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]);
 	m_vActionLoop[m_nIndexTween * TWEEN_TYPE_SIZE + m_nEffectIndex]->IncRef();
-	m_pActionLoop = new ActionLoop(m_pActionSequeue);
-	m_pActionNode->RunAction(m_pActionLoop);
+	ActionLoop* pActionLoop = new ActionLoop(pActionSequeue);
+	m_pActionNode->RunAction(pActionLoop);
 }
 

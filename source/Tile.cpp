fcdaa951c2ba4e85e542788a6e3d66285f961149
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-09-23 04:10:10 +0000

    + load texture from memory + multi-cast-event for any object * fix node UpdateMatrix bug

--- source/Tile.cpp
+++ source/Tile.cpp
@@ -10,35 +10,47 @@
 #include <IRenderer2D.h>
 #include <INode.h>
 
-Tile::Tile(const IMAGE_PIECE* pImagePiece, const Vector3& Pos/* = IMATH::VERTOR3_ZERO*/)
+Tile::Tile(const IMAGE_PIECE* pImagePiece)
 {
 	m_pImagePiece = pImagePiece;
-	m_vPos = Pos;
-	m_bVisual= true;
+	m_bVisible= true;
+
 	m_pShader = IShaderMgr::GetInstance().CreateShader(SSI_DEFAULT);
 }
 
 Tile::~Tile()
 {
-	//TODO:
 	SAFE_RELEASE(m_pShader);
-
-}
-void Tile::Render()
-{
-	INode* pNode = GetParentNode();
-	if (!pNode) return;
-	if(!m_bVisual) return;
-	m_pShader->SetTexture("u_texture",m_pImagePiece->pTexture);
-	IRenderer2D::GetInstance().SetModelViewMatrix(pNode->GetFinalMatrix());
-	m_pShader->SetMatrix4x4("u_matModelViewProj", IRenderer2D::GetInstance().GetFinalMatrixTranspose());
-	IRenderer2D::GetInstance().DrawRect(0.0f, 0.0f, m_pImagePiece, m_pShader);
-	
 }
 
 void Tile::Update(float dt)
 {
 	//TODO:
+}
+
+void Tile::Render()
+{
+	if(!IsVisible()) return;
+
+	INode* pNode = GetParentNode();
+	if (!pNode) return;
+
+	m_pShader->SetTexture("u_texture",m_pImagePiece->pTexture);
+
+	IRenderer2D::GetInstance().SetModelViewMatrix(pNode->GetFinalMatrix());
+	m_pShader->SetMatrix4x4("u_matModelViewProj", IRenderer2D::GetInstance().GetFinalMatrixTranspose());
+
+	IRenderer2D::GetInstance().DrawRect(0.0f, 0.0f, m_pImagePiece, m_pShader);
+}
+
+void Tile::SetVisible(bool bVisible)
+{
+	m_bVisible = bVisible;
+}
+
+bool Tile::IsVisible() const
+{
+	return m_bVisible;
 }
 
 INode* Tile::GetParentNode()
@@ -49,9 +61,4 @@ INode* Tile::GetParentNode()
 	if (pObject->GetRtti()->IsDerived(INode::__RttiData())) return (INode*)pObject;
 
 	return NULL;
-}
-
-void Tile::SetVisual(bool bVisual)
-{
-	m_bVisual = bVisual;
 }

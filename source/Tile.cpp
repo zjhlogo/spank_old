1a59bf50f1ec80f198edfc27b0ea2b98ab123a54
wbaoqing <wbaoqing@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-10-11 01:00:30 +0000

    +Add the SetColor in the Tile

--- source/Tile.cpp
+++ source/Tile.cpp
@@ -12,7 +12,7 @@
 
 const char* Tile::pszShaderFileName[] = 
 {
-	"default_shader.xml",
+	"trans_shader.xml",
 	"Gray_shader.xml"
 };
 
@@ -22,7 +22,8 @@ Tile::Tile(const IMAGE_PIECE* pImagePiece)
 	m_bVisible= true;
 	m_nWidth = pImagePiece->width;
 	m_nHeight = pImagePiece->height;
-	m_pShader = IShaderMgr::GetInstance().CreateShader(SSI_DEFAULT);
+	m_vColor = IMath::VEC4_ONE;
+	m_pShader = IShaderMgr::GetInstance().CreateShader("trans_shader.xml");
 	m_eType = NORMAL;
 }
 
@@ -42,9 +43,9 @@ void Tile::Render()
 
 	INode* pNode = GetParentNode();
 	if (!pNode) return;
-
 	m_pShader->SetTexture("u_texture",m_pImagePiece->pTexture);
-
+	if(m_eType == NORMAL)
+		m_pShader->SetVector4("u_color", m_vColor);
 	IRenderer2D::GetInstance().SetModelViewMatrix(pNode->GetFinalMatrix());
 	m_pShader->SetMatrix4x4("u_matModelViewProj", IRenderer2D::GetInstance().GetFinalMatrixTranspose());
 
@@ -68,6 +69,12 @@ void Tile::SetSize( float width, float height )
 void Tile::SetVisible(bool bVisible)
 {
 	m_bVisible = bVisible;
+}
+
+void Tile::SetColor(const Vector4& vColor)
+{
+	//TODO:
+	m_vColor = vColor;
 }
 
 float Tile::GetWidth() const

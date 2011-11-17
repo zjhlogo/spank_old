0789fb415ff3f5b1b80a1f706c9b61d4b1b22f44
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-11-17 05:31:13 +0000

    + mac network support

---
+++ source/platforms/mac/osui/OSUISystem_Mac_Impl.mm
@@ -0,0 +1,47 @@
+/*!
+ * \file OSUISystem_Mac_Impl.mm
+ * \date 11-10-2011
+ * 
+ * 
+ * \author zjhlogo (zjhlogo@gmail.com)
+ */
+#import "OSUISystem_Mac_Impl.h"
+#import "SpankAppDelegate.h"
+
+IOSUISystem& IOSUISystem::GetInstance()
+{
+	static OSUISystem_Mac_Impl s_OSUISystem_Mac_Impl;
+	return s_OSUISystem_Mac_Impl;
+}
+
+OSUISystem_Mac_Impl::OSUISystem_Mac_Impl()
+{
+	// TODO: 
+}
+
+OSUISystem_Mac_Impl::~OSUISystem_Mac_Impl()
+{
+	// TODO: 
+}
+
+bool OSUISystem_Mac_Impl::Initialize()
+{
+	return true;
+}
+
+void OSUISystem_Mac_Impl::Terminate()
+{
+	// TODO: 
+}
+
+int OSUISystem_Mac_Impl::CreateButton(ISurfaceView* pSurfaceView, int x, int y, int width, int height, const char* pszText)
+{
+	int nViewId = pSurfaceView->GetViewId();
+	return [[SpankAppDelegate GetInstance] createButton:nViewId x:x y:y width:width height:height text:pszText];
+}
+
+void OSUISystem_Mac_Impl::DestroyButton(ISurfaceView* pSurfaceView, int nId)
+{
+	int nViewId = pSurfaceView->GetViewId();
+	[[SpankAppDelegate GetInstance] destroyButton:nViewId buttonId:nId];
+}

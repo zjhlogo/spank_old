25f302e65d8e1b72549b6677f60b48a3c6257e26
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-11-17 05:31:13 +0000

    + mac network support

--- trunk/source/platforms/mac/net/NetMgr_Mac_Impl.h
+++ trunk/source/platforms/mac/net/NetMgr_Mac_Impl.h
@@ -8,5 +8,40 @@
 #ifndef __NETMGR_MAC_IMPL_H__
 #define __NETMGR_MAC_IMPL_H__
 
+#include <net/INetMgr.h>
+
+class NetMgr_Mac_Impl : public INetMgr
+{
+public:
+	DECLARE_RTTI(NetMgr_Mac_Impl, INetMgr);
+	
+	NetMgr_Mac_Impl();
+	virtual ~NetMgr_Mac_Impl();
+	
+	virtual bool Initialize();
+	virtual void Terminate();
+	
+	virtual void Update(float dt);
+	
+	virtual bool ConnectToServer(const char* ip, ushort port);
+	virtual bool ConnectToServer(uint ip, ushort port);
+	virtual void Disconnect();
+	
+	virtual bool SendNetMessage(INetMsgBase* pMsg);
+	
+	virtual void SetNetMsgHandler(INetMsgHandler* pHandler);
+	virtual INetMsgHandler* GetNetMsgHandler();
+	virtual bool HandlerDataBlock(const void* pData, int nLength);
+
+private:
+	void ReceiveOnce();
+	void SendOnce();
+	
+private:
+	int m_nSocket;
+	StreamWriter m_SendBuffer;
+	INetMsgHandler* m_pNetMsgHandler;
+	
+};
 
 #endif // __NETMGR_MAC_IMPL_H__

b8009ab511f3969b6a04726fab2725eb7bcf8158
zjhlogo <zjhlogo@181a2263-6311-9be6-e15f-e484ee8db88e>
2011-11-17 05:10:35 +0000

    + mac socket support (not implement yet)

---
+++ trunk/source/platforms/mac/net/NetMgr_Mac_Impl.h
@@ -0,0 +1,12 @@
+/*!
+ * \file NetMgr_Mac_Impl.h
+ * \date 11-17-2011 12:28:51
+ * 
+ * 
+ * \author zjhlogo (zjhlogo@gmail.com)
+ */
+#ifndef __NETMGR_MAC_IMPL_H__
+#define __NETMGR_MAC_IMPL_H__
+
+
+#endif // __NETMGR_MAC_IMPL_H__
